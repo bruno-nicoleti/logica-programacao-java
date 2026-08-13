f5ff894945923283a1a6f9bd0d56e6168a3e7ca0
bruno-nicoleti <brunicoleti@gmail.com>
2026-08-13 00:21:28 -0300

    Create hora.jogo.java
    Calcula a quantidade de horas que um individuo ficou jogando, sendo que pode começar em um dia e terminar no outro

---
+++ hora.jogo.java
@@ -0,0 +1,19 @@
+void main() {
+
+	Scanner sc = new Scanner(System.in);
+	
+	int horaInicial = sc.nextInt();
+	int horaFinal = sc.nextInt();
+	
+	int duracao;
+	
+	if (horaInicial < horaFinal) {
+		duracao = horaFinal - horaInicial;
+	}
+	else {
+		duracao = 24 - horaInicial + horaFinal;
+	}
+	System.out.println("O JOGO DUROU " + duracao + "HORA(S)");
+	
+	sc.close();
+}
